37dfe41f0adfbede4a3b7424f4c9e271e81ee6a4
Marcel Oomens <mail@marceloomens.com>
2021-01-30 09:10:33 +0100

    Provision elderjs
    Provision elderjs and move html into Svelte components
    
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

---
+++ src/routes/home/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Tile from '../../components/Tile.svelte';
+</script>
+
+<section class="mosaic">
+  <Tile anchor={"pecan"} image={"mosaic-1.JPG"} alt={"Pecan cake"} />
+  <Tile anchor={"passion"} image={"mosaic-1.JPG"} alt={"Passion cake"} />
+  <Tile anchor={"new-year"} image={"mosaic-1.JPG"} alt={"New Year's cake"}/ />
+</section>
