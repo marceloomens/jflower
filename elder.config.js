93e9e779cc42c7c188efaa7aedb2e6020fa58160
Marcel Oomens <mail@marceloomens.com>
2021-01-30 15:23:45 +0100

    Update config files
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- elder.config.js
+++ elder.config.js
@@ -19,9 +19,9 @@ module.exports = {
     // disable: ['elderWriteHtmlFileToPublic'], // this is used to disable internal hooks. Uncommenting this would disabled writing your files on build.
   },
   plugins: {
-    '@elderjs/plugin-markdown': {
-      routes: ['blog'],
-    },
+    // '@elderjs/plugin-markdown': {
+    //   routes: ['blog'],
+    // },
     '@elderjs/plugin-browser-reload': {
       // this reloads your browser when nodemon restarts your server.
       port: 8080,
