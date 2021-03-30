f7a4a4eedb8bb302a4d31d5b9914d2cdca7b09a8
Marcel Oomens <mail@marceloomens.com>
2021-03-30 18:36:38 +0200

    WIP: Elder.js config for Netlify CMS
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -20,9 +20,7 @@ section {
 
 <h2 class="font-display text-center text-xl">Cake Menu</h2>
 <section class="grid">
-{#if data.cakes}
-  {#each data.cakes as cake, i}
-  <Tile id={i} {...cake} />
-  {/each}
-{/if}
+{#each data.markdown.products as product, i}
+  <Tile id={i} {...product.frontmatter} />
+{/each}
 </section>
---
+++ src/routes/products/passion-cake.md
@@ -0,0 +1,5 @@
+---
+name: '2021 New Year Passion cake'
+description: 'New year specialty passion fruit cheese cake with chiffon, cream cheese, and passion fruit two ways'
+image: 'passion.jpg'
+---
