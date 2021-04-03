d5d38fe8f0517d3ab6f84c4ece4e7d370fa0507a
Marcel Oomens <mail@marceloomens.com>
2021-04-03 16:12:29 +0200

    Initial commit
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

---
+++ src/components/cart.js
@@ -0,0 +1,48 @@
+import { writable } from 'svelte/store'
+
+const DEFAULT = 0;
+
+const stores = new Map();
+
+function createStore( identifier ) {
+
+  const value  = localStorage.getItem( identifier );
+  const { subscribe, set, update } = writable( value || DEFAULT );
+
+  subscribe(v => {
+    localStorage.setItem( identifier, v )
+  });
+
+  function add( n ) {
+    update(v => v + n);
+  }
+
+  function remove( n ) {
+    update(v => v < n ? 0 : v - n);
+  }
+
+  return {
+    subscribe,
+    add,
+    remove
+  };
+}
+
+export function getCart( a, b ) {
+
+  if ( typeof a === "undefined" )
+    throw new Error( "(cart.js) `getCart( a, b )` requires at least one parameter." );
+  const product = b ?? a;
+  const variant = b ? a : "default";
+
+  const identifier = `cart/${product}/${variant}`;
+
+  if ( stores.has(identifier) ) {
+    return stores.get(identifier);
+  }
+
+  const store = createStore( identifier );
+  stores.set( identifier, store )
+
+  return store;
+}
