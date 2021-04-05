1697f2fb7166512dcca1f238e05a494bc21ac2e4
Marcel Oomens <mail@marceloomens.com>
2021-04-05 21:06:24 +0200

    Layout improvements
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- src/routes/home/route.js
+++ src/routes/home/route.js
@@ -4,6 +4,18 @@ module.exports = {
   // the permalink function takes a 'request' object and returns a relative permalink. In this case "/"
   permalink: ({ request }) => request.slug,
   data: ({ data }) => {
+
+    const products = (data.markdown.products)
+      .map(p => p.frontmatter);
+    products.filter( a => a.promoted );
+    products.sort( (a, b) => {
+      if (a.order > b.order ) { return 1; }
+      else if (a.order < b.order ) { return -1; }
+      else { return 0; }
+    } );
+
+    data.products = products;
+
     return data
   },
   // template: 'Home.svelte' // this is auto-detected.
