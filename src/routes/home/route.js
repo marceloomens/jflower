d94e699d889dd5ebb70358a08b9ff9dff2f7a6d1
Marcel Oomens <mail@marceloomens.com>
2021-11-08 22:54:42 +0100

    Availability affect ordering
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- src/routes/home/route.js
+++ src/routes/home/route.js
@@ -4,19 +4,25 @@ module.exports = {
   // the permalink function takes a 'request' object and returns a relative permalink. In this case "/"
   permalink: ({ request }) => request.slug,
   data: ({ data }) => {
-
-    const products = (data.markdown.products)
-      .map(p => p.frontmatter);
-    products.filter( a => a.promoted );
-    products.sort( (a, b) => {
-      if (a.order > b.order ) { return 1; }
-      else if (a.order < b.order ) { return -1; }
-      else { return 0; }
-    } );
+    const products = data.markdown.products.map((p) => p.frontmatter);
+    products.filter((a) => a.promoted);
+    products.sort((a, b) => {
+      if (a.available && !b.available) {
+        return -1;
+      } else if (!a.available && b.available) {
+        return 1;
+      } else if (a.order > b.order) {
+        return 1;
+      } else if (a.order < b.order) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
 
     data.products = products;
 
-    return data
+    return data;
   },
   // template: 'Home.svelte' // this is auto-detected.
   // layout: 'Layout.svelte' // this is auto-detected.
